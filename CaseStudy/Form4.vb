55da49232637ad450f674d113f5b6490a56a67e3
chillman203 <chillman203@gmail.com>
2025-05-02 12:48:56 +0800

    Form4 Receipt Added

---
+++ CaseStudy/Form4.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form4
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label5.Click
+
+    End Sub
+
+    Private Sub TextBox4_TextChanged(sender As Object, e As EventArgs) Handles TextBox4.TextChanged
+
+    End Sub
+End Class
