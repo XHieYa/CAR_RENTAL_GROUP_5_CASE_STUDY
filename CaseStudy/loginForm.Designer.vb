6417572b1308763822b5cf40e6cb1f2589602968
Twylaven <maryjazlynpineda@gmail.com>
2025-05-09 10:55:51 +0800

    Login WIP

--- CaseStudy/loginForm.Designer.vb
+++ CaseStudy/loginForm.Designer.vb
@@ -60,24 +60,28 @@ Partial Class loginForm
         ' 
         GroupBox1.Controls.Add(cbShowPassLI)
         GroupBox1.Controls.Add(llblCreateAccount)
+        GroupBox1.Controls.Add(Label1)
         GroupBox1.Controls.Add(BtnLogin)
         GroupBox1.Controls.Add(txtpass)
         GroupBox1.Controls.Add(txtuser)
         GroupBox1.Controls.Add(Label4)
         GroupBox1.Controls.Add(Label3)
-        GroupBox1.Controls.Add(Label1)
-        GroupBox1.Location = New Point(6, -3)
+        GroupBox1.Location = New Point(175, 13)
+        GroupBox1.Margin = New Padding(3, 4, 3, 4)
         GroupBox1.Name = "GroupBox1"
-        GroupBox1.Size = New Size(393, 447)
+        GroupBox1.Padding = New Padding(3, 4, 3, 4)
+        GroupBox1.Size = New Size(497, 664)
         GroupBox1.TabIndex = 0
         GroupBox1.TabStop = False
         ' 
         ' cbShowPassLI
         ' 
         cbShowPassLI.AutoSize = True
-        cbShowPassLI.Location = New Point(20, 109)
+        cbShowPassLI.Font = New Font("Footlight MT Light", 9F)
+        cbShowPassLI.Location = New Point(23, 145)
+        cbShowPassLI.Margin = New Padding(3, 4, 3, 4)
         cbShowPassLI.Name = "cbShowPassLI"
-        cbShowPassLI.Size = New Size(108, 19)
+        cbShowPassLI.Size = New Size(122, 19)
         cbShowPassLI.TabIndex = 7
         cbShowPassLI.Text = "Show Password"
         cbShowPassLI.UseVisualStyleBackColor = True
@@ -85,62 +89,68 @@ Partial Class loginForm
         ' llblCreateAccount
         ' 
         llblCreateAccount.AutoSize = True
-        llblCreateAccount.Location = New Point(6, 131)
+        llblCreateAccount.Font = New Font("Footlight MT Light", 9F)
+        llblCreateAccount.Location = New Point(7, 175)
         llblCreateAccount.Name = "llblCreateAccount"
-        llblCreateAccount.Size = New Size(89, 15)
+        llblCreateAccount.Size = New Size(101, 15)
         llblCreateAccount.TabIndex = 6
         llblCreateAccount.TabStop = True
         llblCreateAccount.Text = "Create Account"
         ' 
         ' BtnLogin
         ' 
-        BtnLogin.Location = New Point(252, 119)
+        BtnLogin.Location = New Point(288, 159)
+        BtnLogin.Margin = New Padding(3, 4, 3, 4)
         BtnLogin.Name = "BtnLogin"
-        BtnLogin.Size = New Size(75, 23)
+        BtnLogin.Size = New Size(86, 31)
         BtnLogin.TabIndex = 5
         BtnLogin.Text = "Log-in"
         BtnLogin.UseVisualStyleBackColor = True
         ' 
         ' txtpass
         ' 
-        txtpass.Location = New Point(99, 80)
+        txtpass.Location = New Point(152, 99)
+        txtpass.Margin = New Padding(3, 4, 3, 4)
         txtpass.Name = "txtpass"
         txtpass.PasswordChar = "*"c
-        txtpass.Size = New Size(123, 23)
+        txtpass.Size = New Size(174, 27)
         txtpass.TabIndex = 4
         ' 
         ' txtuser
         ' 
-        txtuser.Location = New Point(99, 51)
+        txtuser.Location = New Point(152, 67)
+        txtuser.Margin = New Padding(3, 4, 3, 4)
         txtuser.Name = "txtuser"
-        txtuser.Size = New Size(153, 23)
+        txtuser.Size = New Size(174, 27)
         txtuser.TabIndex = 3
         ' 
         ' Label4
         ' 
         Label4.AutoSize = True
-        Label4.Location = New Point(6, 83)
+        Label4.Font = New Font("Footlight MT Light", 9F)
+        Label4.Location = New Point(7, 111)
         Label4.Name = "Label4"
-        Label4.Size = New Size(60, 15)
+        Label4.Size = New Size(67, 15)
         Label4.TabIndex = 2
         Label4.Text = "Password:"
         ' 
         ' Label3
         ' 
         Label3.AutoSize = True
-        Label3.Location = New Point(6, 59)
+        Label3.Font = New Font("Footlight MT Light", 9F)
+        Label3.Location = New Point(7, 79)
         Label3.Name = "Label3"
-        Label3.Size = New Size(93, 15)
+        Label3.Size = New Size(105, 15)
         Label3.TabIndex = 1
         Label3.Text = "Account Name: "
         ' 
         ' Label1
         ' 
         Label1.AutoSize = True
-        Label1.Font = New Font("Microsoft Sans Serif", 15.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
-        Label1.Location = New Point(6, 19)
+        Label1.Font = New Font("Felix Titling", 16.2F, FontStyle.Bold)
+        Label1.Location = New Point(188, 24)
         Label1.Name = "Label1"
-        Label1.Size = New Size(78, 25)
+        Label1.Size = New Size(116, 33)
         Label1.TabIndex = 0
         Label1.Text = "Log-In"
         ' 
@@ -148,6 +158,7 @@ Partial Class loginForm
         ' 
         GroupBox2.Controls.Add(cbShowPasswordSI)
         GroupBox2.Controls.Add(txtEmail)
+        GroupBox2.Controls.Add(Label2)
         GroupBox2.Controls.Add(Label12)
         GroupBox2.Controls.Add(llblHaveAnAccount)
         GroupBox2.Controls.Add(Label11)
@@ -165,45 +176,51 @@ Partial Class loginForm
         GroupBox2.Controls.Add(Label7)
         GroupBox2.Controls.Add(Label6)
         GroupBox2.Controls.Add(Label5)
-        GroupBox2.Controls.Add(Label2)
-        GroupBox2.Location = New Point(405, -3)
+        GroupBox2.Location = New Point(786, 13)
+        GroupBox2.Margin = New Padding(3, 4, 3, 4)
         GroupBox2.Name = "GroupBox2"
-        GroupBox2.Size = New Size(393, 447)
+        GroupBox2.Padding = New Padding(3, 4, 3, 4)
+        GroupBox2.Size = New Size(497, 664)
         GroupBox2.TabIndex = 1
         GroupBox2.TabStop = False
         ' 
         ' cbShowPasswordSI
         ' 
         cbShowPasswordSI.AutoSize = True
-        cbShowPasswordSI.Location = New Point(309, 164)
+        cbShowPasswordSI.Font = New Font("Footlight MT Light", 9F)
+        cbShowPasswordSI.Location = New Point(353, 219)
+        cbShowPasswordSI.Margin = New Padding(3, 4, 3, 4)
         cbShowPasswordSI.Name = "cbShowPasswordSI"
-        cbShowPasswordSI.Size = New Size(76, 34)
+        cbShowPasswordSI.Size = New Size(85, 34)
         cbShowPasswordSI.TabIndex = 22
         cbShowPasswordSI.Text = "Show" & vbCrLf & "Password"
         cbShowPasswordSI.UseVisualStyleBackColor = True
         ' 
         ' txtEmail
         ' 
-        txtEmail.Location = New Point(105, 85)
+        txtEmail.Location = New Point(120, 113)
+        txtEmail.Margin = New Padding(3, 4, 3, 4)
         txtEmail.Name = "txtEmail"
-        txtEmail.Size = New Size(198, 23)
+        txtEmail.Size = New Size(226, 27)
         txtEmail.TabIndex = 20
         ' 
         ' Label12
         ' 
         Label12.AutoSize = True
-        Label12.Location = New Point(6, 88)
+        Label12.Font = New Font("Footlight MT Light", 9F)
+        Label12.Location = New Point(7, 117)
         Label12.Name = "Label12"
-        Label12.Size = New Size(89, 15)
+        Label12.Size = New Size(104, 15)
         Label12.TabIndex = 21
         Label12.Text = "E-Mail Address:"
         ' 
         ' llblHaveAnAccount
         ' 
         llblHaveAnAccount.AutoSize = True
-        llblHaveAnAccount.Location = New Point(6, 292)
+        llblHaveAnAccount.Font = New Font("Footlight MT Light", 9F)
+        llblHaveAnAccount.Location = New Point(7, 389)
         llblHaveAnAccount.Name = "llblHaveAnAccount"
-        llblHaveAnAccount.Size = New Size(182, 15)
+        llblHaveAnAccount.Size = New Size(212, 15)
         llblHaveAnAccount.TabIndex = 8
         llblHaveAnAccount.TabStop = True
         llblHaveAnAccount.Text = "Have an Account already? Log-in"
@@ -211,17 +228,19 @@ Partial Class loginForm
         ' Label11
         ' 
         Label11.AutoSize = True
-        Label11.Location = New Point(6, 122)
+        Label11.Font = New Font("Footlight MT Light", 9F)
+        Label11.Location = New Point(7, 163)
         Label11.Name = "Label11"
-        Label11.Size = New Size(59, 15)
+        Label11.Size = New Size(67, 15)
         Label11.TabIndex = 18
         Label11.Text = "Fullname:"
         ' 
         ' txtFullName
         ' 
-        txtFullName.Location = New Point(105, 114)
+        txtFullName.Location = New Point(120, 152)
+        txtFullName.Margin = New Padding(3, 4, 3, 4)
         txtFullName.Name = "txtFullName"
-        txtFullName.Size = New Size(198, 23)
+        txtFullName.Size = New Size(226, 27)
         txtFullName.TabIndex = 17
         ' 
         ' cmbSex
@@ -229,128 +248,143 @@ Partial Class loginForm
         cmbSex.DropDownStyle = ComboBoxStyle.DropDownList
         cmbSex.FormattingEnabled = True
         cmbSex.Items.AddRange(New Object() {"Male", "Female", "Non-Binary", "LGBTQ+", "Not Prefered to Say"})
-        cmbSex.Location = New Point(105, 199)
+        cmbSex.Location = New Point(120, 265)
+        cmbSex.Margin = New Padding(3, 4, 3, 4)
         cmbSex.Name = "cmbSex"
-        cmbSex.Size = New Size(198, 23)
+        cmbSex.Size = New Size(226, 28)
         cmbSex.TabIndex = 16
         ' 
         ' dtpDOB
         ' 
-        dtpDOB.Location = New Point(105, 257)
+        dtpDOB.Font = New Font("Footlight MT Light", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        dtpDOB.Location = New Point(120, 343)
+        dtpDOB.Margin = New Padding(3, 4, 3, 4)
         dtpDOB.Name = "dtpDOB"
-        dtpDOB.Size = New Size(198, 23)
+        dtpDOB.Size = New Size(226, 23)
         dtpDOB.TabIndex = 15
         dtpDOB.Value = New Date(2025, 3, 28, 13, 52, 21, 0)
         ' 
         ' txtPassword
         ' 
-        txtPassword.Location = New Point(105, 170)
+        txtPassword.Location = New Point(120, 227)
+        txtPassword.Margin = New Padding(3, 4, 3, 4)
         txtPassword.Name = "txtPassword"
         txtPassword.PasswordChar = "*"c
-        txtPassword.Size = New Size(198, 23)
+        txtPassword.Size = New Size(226, 27)
         txtPassword.TabIndex = 6
         ' 
         ' Label10
         ' 
         Label10.AutoSize = True
-        Label10.Location = New Point(6, 265)
+        Label10.Font = New Font("Footlight MT Light", 9F)
+        Label10.Location = New Point(7, 353)
         Label10.Name = "Label10"
-        Label10.Size = New Size(81, 15)
+        Label10.Size = New Size(98, 15)
         Label10.TabIndex = 6
         Label10.Text = "Date Of  Birth:"
         ' 
         ' BtnSignIn
         ' 
-        BtnSignIn.Location = New Point(286, 292)
+        BtnSignIn.Location = New Point(327, 389)
+        BtnSignIn.Margin = New Padding(3, 4, 3, 4)
         BtnSignIn.Name = "BtnSignIn"
-        BtnSignIn.Size = New Size(75, 23)
+        BtnSignIn.Size = New Size(86, 31)
         BtnSignIn.TabIndex = 6
         BtnSignIn.Text = "Sign-Up"
         BtnSignIn.UseVisualStyleBackColor = True
         ' 
         ' txtAddress
         ' 
-        txtAddress.Location = New Point(105, 228)
+        txtAddress.Location = New Point(120, 304)
+        txtAddress.Margin = New Padding(3, 4, 3, 4)
         txtAddress.Name = "txtAddress"
-        txtAddress.Size = New Size(198, 23)
+        txtAddress.Size = New Size(226, 27)
         txtAddress.TabIndex = 14
         ' 
         ' txtAge
         ' 
-        txtAge.Location = New Point(105, 141)
+        txtAge.Location = New Point(120, 188)
+        txtAge.Margin = New Padding(3, 4, 3, 4)
         txtAge.Name = "txtAge"
-        txtAge.Size = New Size(198, 23)
+        txtAge.Size = New Size(226, 27)
         txtAge.TabIndex = 11
         ' 
         ' txtUsername
         ' 
-        txtUsername.Location = New Point(105, 56)
+        txtUsername.Location = New Point(120, 75)
+        txtUsername.Margin = New Padding(3, 4, 3, 4)
         txtUsername.Name = "txtUsername"
-        txtUsername.Size = New Size(198, 23)
+        txtUsername.Size = New Size(226, 27)
         txtUsername.TabIndex = 6
         ' 
         ' Label9
         ' 
         Label9.AutoSize = True
-        Label9.Location = New Point(6, 236)
+        Label9.Font = New Font("Footlight MT Light", 9F)
+        Label9.Location = New Point(7, 315)
         Label9.Name = "Label9"
-        Label9.Size = New Size(52, 15)
+        Label9.Size = New Size(59, 15)
         Label9.TabIndex = 10
         Label9.Text = "Address:"
         ' 
         ' Label8
         ' 
         Label8.AutoSize = True
-        Label8.Location = New Point(6, 207)
+        Label8.Font = New Font("Footlight MT Light", 9F)
+        Label8.Location = New Point(7, 276)
         Label8.Name = "Label8"
-        Label8.Size = New Size(28, 15)
+        Label8.Size = New Size(33, 15)
         Label8.TabIndex = 9
         Label8.Text = "Sex:"
         ' 
         ' Label7
         ' 
         Label7.AutoSize = True
-        Label7.Location = New Point(6, 178)
+        Label7.Font = New Font("Footlight MT Light", 9F)
+        Label7.Location = New Point(7, 237)
         Label7.Name = "Label7"
-        Label7.Size = New Size(60, 15)
+        Label7.Size = New Size(67, 15)
         Label7.TabIndex = 8
         Label7.Text = "Password:"
         ' 
         ' Label6
         ' 
         Label6.AutoSize = True
-        Label6.Location = New Point(6, 149)
+        Label6.Font = New Font("Footlight MT Light", 9F)
+        Label6.Location = New Point(7, 199)
         Label6.Name = "Label6"
-        Label6.Size = New Size(34, 15)
+        Label6.Size = New Size(39, 15)
         Label6.TabIndex = 7
         Label6.Text = "Age: "
         ' 
         ' Label5
         ' 
         Label5.AutoSize = True
-        Label5.Location = New Point(6, 59)
+        Label5.Font = New Font("Footlight MT Light", 9F)
+        Label5.Location = New Point(7, 79)
         Label5.Name = "Label5"
-        Label5.Size = New Size(93, 15)
+        Label5.Size = New Size(105, 15)
         Label5.TabIndex = 6
         Label5.Text = "Account Name: "
         ' 
         ' Label2
         ' 
         Label2.AutoSize = True
-        Label2.Font = New Font("Microsoft Sans Serif", 15.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
-        Label2.Location = New Point(6, 19)
+        Label2.Font = New Font("Felix Titling", 16.2F, FontStyle.Bold)
+        Label2.Location = New Point(199, 24)
         Label2.Name = "Label2"
-        Label2.Size = New Size(86, 25)
+        Label2.Size = New Size(121, 33)
         Label2.TabIndex = 1
         Label2.Text = "Sign-In"
         ' 
         ' loginForm
         ' 
-        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(800, 450)
+        ClientSize = New Size(1326, 690)
         Controls.Add(GroupBox2)
         Controls.Add(GroupBox1)
+        Margin = New Padding(3, 4, 3, 4)
         Name = "loginForm"
         Text = "Form1"
         GroupBox1.ResumeLayout(False)
